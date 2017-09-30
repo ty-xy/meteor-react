93c9ab0f4c86bb2adc4e600d6b0af5721081539d
王亚星 <funnycoderstar@gmail.com>
2017-09-30 21:44:34 +0800

    feat: complete register page

--- imports/client/views/login/register.jsx
+++ imports/client/views/login/register.jsx
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
+import '../../styles/view/login/register';
 
 class Register extends Component {
     render() {
         return (
-            <div className="ejianlian-form-wrap">
+            <div className="ejianlian-form-wrap ejianlian-register">
                 <div className="form-content-wrap container-middle">
-                    <div className="login-title container-title">
+                    <div className="register-title container-title">
                         注册e建联
                     </div>
                     <div className="form-content">
-                        这是内容
+                        <ul className="register-step">
+                            <li>
+                                <input type="text" placeholder="手机号" />
+                            </li>
+                            <li>
+                                <input type="text" placeholder="验证码" />
+                                <p className="obtainCode">获取验证码</p>
+                            </li>
+                            <li>
+                                <input type="text" placeholder="密码" />
+                            </li>
+                            <li>
+                                <input type="text" placeholder="您的名字" />
+                            </li>
+                        </ul>
+                        <div className="register-btn">完成</div>
+                        <div className="login-by-other">
+                            <div className="login-style-wrap">
+                                <p className="login-line" />
+                                <p> 第三方登录</p>
+                                <p className="login-line" />
+
+                            </div>
+                            <div className="login-style" >
+                                <p><i className="icon">&#xe601;</i></p>
+                                <p><i className="icon icon-wechat">&#xe6f6;</i></p>
+                            </div>
+                        </div>
+                        <p className="agreeProto">注册即同意知工应用<span>服务条款</span></p>
                     </div>
+
                 </div>
             </div>
         );
