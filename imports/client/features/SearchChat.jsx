6738ab88e1df5ded358560f338fb9192a6883283
王亚星 <funnycoderstar@gmail.com>
2017-11-15 12:35:40 +0800

    feat: 删除搜索聊天记录功能

--- imports/client/features/SearchChat.jsx
+++ imports/client/features/SearchChat.jsx
@@ -3,8 +3,6 @@ import { Meteor } from 'meteor/meteor';
 import { Input, Card } from 'antd';
 
 import Avatar from '../components/Avatar';
-import Group from '../../schema/group';
-import fields from '../../util/fields';
 import eventUtil from '../../util/eventUtil';
 
 const Search = Input.Search;
@@ -15,7 +13,6 @@ class SearchChat extends Component {
         this.state = {
             friends: [],
             groups: [],
-            messages: [],
             showSearchResult: false,
             searchValue: '',
         };
@@ -30,35 +27,13 @@ class SearchChat extends Component {
         } </span>);
     }
     search = (value) => {
-        // console.log(value);
         this.setState({
             searchValue: value,
         });
         Meteor.call('searchChat', value, async (err, result) => {
-            // console.log(7777, result);
             this.setState({
                 friends: result.friends,
                 groups: result.groups,
-                messages: result.message,
-            });
-            Meteor.subscribe('users');
-            Meteor.subscribe('group');
-            await this.state.messages.forEach((k) => {
-                if (k.to.length <= 17) {
-                    k.chat = Group.findOne({ _id: k.to }, { fields: fields.searchGroup });
-                    return;
-                    // 群聊天
-                } else if (k.to.length >= 34) {
-                    const userId = k.to.slice(0, k.to.length / 2);
-                    if (userId !== Meteor.userId()) {
-                        // 用户聊天
-                        k.chat = Meteor.users.findOne({ _id: userId }, { fields: fields.searchUser });
-                        return;
-                    }
-                    k.chat = Meteor.users.findOne({ _id: k.from }, { fields: fields.searchUser });
-                    return;
-                }
-                console.log('to字段Id的长度', k.to.length);
             });
             await this.setState({
                 showSearchResult: true,
@@ -79,7 +54,7 @@ class SearchChat extends Component {
         return (
             <div className="serach-chat">
                 <div className="certain-category-search-wrapper">
-                    <Search onSearch={this.search} placeholder="好友,群组,聊天记录" />
+                    <Search onSearch={this.search} placeholder="好友,群组" />
                 </div>
                 {
                     this.state.showSearchResult ?
@@ -117,43 +92,6 @@ class SearchChat extends Component {
                                                         (<div className="user-item-card" key={group._id}>
                                                             <Avatar name={group.name} avatar={group.avatar} />
                                                             <div className="user-name">{this.getHighlightedText(group.name, this.state.searchValue)}</div>
-                                                        </div>),
-                                                    )
-                                                    :
-                                                    <div>暂无群组搜索结果</div>
-                                            }
-                                        </Card>
-                                        <Card title="聊天记录" extra={<span>2</span>} >
-                                            {
-                                                this.state.messages.length > 0 ?
-                                                    this.state.messages.map(message =>
-                                                        (<div className="user-item-card" key={message._id}>
-                                                            {
-                                                                message.chat && message.chat.name ?
-                                                                    <Avatar name={message.chat.name} avatar={message.chat.avatar} />
-                                                                    :
-                                                                    message.chat && message.chat.profile ?
-                                                                        <Avatar avatarColor={message.chat.profile.avatarColor} name={message.chat.profile.name} avatar={message.chat.profile.avatar} />
-                                                                        :
-                                                                        null
-
-                                                            }
-
-                                                            <div>
-                                                                {
-                                                                    message.chat && message.chat.name ?
-                                                                        <div>{message.chat.name}</div>
-                                                                        :
-                                                                        message.chat && message.chat.profile ?
-                                                                            <div>{message.chat.profile.name}</div>
-                                                                            :
-                                                                            <div>用户名</div>
-
-                                                                }
-                                                                <div className="chat-record">{this.getHighlightedText(message.content, this.state.searchValue)}</div>
-                                                            </div>
-
-
                                                         </div>),
                                                     )
                                                     :
--- server/search/searchChat.js
+++ server/search/searchChat.js
@@ -1,10 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
-import Messages from '../../imports/schema/message';
 import Group from '../../imports/schema/group';
-
 import UserUtil from '../../imports/util/user';
-import IdUtil from '../../imports/util/id';
 import fields from '../../imports/util/fields';
 
 Meteor.methods({
@@ -12,12 +9,6 @@ Meteor.methods({
         // 该用户的好友,群组,聊天记录
         const selfGroup = UserUtil.getGroups();
         const selfFriend = UserUtil.getFriends();
-        const friendMessage = selfFriend.map(i => IdUtil.merge(Meteor.userId(), i));
-        const chatMessageId = [...selfGroup, ...friendMessage];
-        const message = Messages.find(
-            { content: { $regex: `${pattern}` }, to: { $in: chatMessageId }, type: 'text' },
-            { fields: fields.searchMessage },
-        ).fetch();
         const friends = Meteor.users.find(
             { 'profile.name': { $regex: `${pattern}` }, _id: { $in: selfFriend } },
             { fields: fields.searchUser },
@@ -27,7 +18,6 @@ Meteor.methods({
             { fields: fields.searchGroup },
         ).fetch();
         const searchResult = {
-            message,
             friends,
             groups,
         };
