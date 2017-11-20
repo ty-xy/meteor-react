29621653f0eca8e232af965e79157e114dff5986
zg gwt <zg-gwt@zg18.com>
2017-11-20 09:54:34 +0800

    feature: menu arrow down up

--- imports/client/views/backEnd/organization/Organization.jsx
+++ imports/client/views/backEnd/organization/Organization.jsx
@@ -13,8 +13,12 @@ class Organization extends PureComponent {
         e.preventDefault();
         this.setState({ depActive });
     }
+    // 公司部门收起
+    showMenu = () => {
+        this.setState({ showMenu: !this.state.showMenu });
+    }
     render() {
-        const { depActive } = this.state;
+        const { depActive, showMenu } = this.state;
         return (
             <div className="e-mg-organization">
                 <Row gutter={16} type="flex" justify="space-between" align="stretch">
@@ -29,11 +33,18 @@ class Organization extends PureComponent {
                             <a href="" className={classnames('e-mg-organization-company', { 'dep-active': depActive === '' })} onClick={e => this.handleTabDep(e, '')}>
                                 <img src="http://oxldjnom8.bkt.clouddn.com/avatar_DQn6qYhEH3PejeDJf_1510912617360.png" alt="" />
                                 中亿集团有限公司
+                                <i className={classnames('iconfont icon-jiantou-copy', { arrowDown: showMenu })} onClick={this.showMenu} />
                                 <span>23</span>
                             </a>
-                            <a href="" className={classnames({ 'dep-active': depActive === '财政部' })} onClick={e => this.handleTabDep(e, '财政部')}>财政部 <span>6</span></a>
-                            <a href="" className={classnames({ 'dep-active': depActive === '技术部' })} onClick={e => this.handleTabDep(e, '技术部')}>技术部 <span>2</span></a>
-                            <a href="" className={classnames({ 'dep-active': depActive === '设计部' })} onClick={e => this.handleTabDep(e, '设计部')}>设计部 <span>4</span></a>
+                            {
+                                showMenu ? (
+                                    <div>
+                                        <a href="" className={classnames({ 'dep-active': depActive === '财政部' })} onClick={e => this.handleTabDep(e, '财政部')}>财政部 <span>6</span></a>
+                                        <a href="" className={classnames({ 'dep-active': depActive === '技术部' })} onClick={e => this.handleTabDep(e, '技术部')}>技术部 <span>2</span></a>
+                                        <a href="" className={classnames({ 'dep-active': depActive === '设计部' })} onClick={e => this.handleTabDep(e, '设计部')}>设计部 <span>4</span></a>
+                                    </div>
+                                ) : null
+                            }
                         </div>
                     </Col>
                     <Col span={18} className="e-mg-organization-card e-mg-organization-right">
