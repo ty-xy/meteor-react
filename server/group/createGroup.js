934a681a49907ac1ab59b83084404cdc5fbe1c5d
王亚星 <funnycoderstar@gmail.com>
2017-11-24 10:24:40 +0800

    feat:添加部分归属的公司字段

--- server/group/createGroup.js
+++ server/group/createGroup.js
@@ -3,8 +3,8 @@ import { Meteor } from 'meteor/meteor';
 import Group from '../../imports/schema/group';
 
 Meteor.methods({
-    // name: 群聊/团队名称, members: 群聊成员, type: group/team 区分是个人群聊和团队群聊
-    createGroup({ name, members, type = 'group' }) {
+    // name: 群聊/团队名称, members: 群聊成员, type: group/team 区分是个人群聊和团队群聊, superiorId: 团队里面的部门所属的公司ID
+    createGroup({ name, members, type = 'group', superiorId = '' }) {
         const newGroup = {
             createdAt: new Date(),
             name,
@@ -19,6 +19,7 @@ Meteor.methods({
                 createdAt: new Date(),
             },
             type,
+            superiorId,
         };
         Group.schema.validate(newGroup);
         const groupId = Group.insert(newGroup);
