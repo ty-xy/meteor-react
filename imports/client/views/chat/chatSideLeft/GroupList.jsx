983958f3393aca412ca357761ceca47abf4c62cc
王亚星 <funnycoderstar@gmail.com>
2017-11-30 12:30:28 +0800

    feat: 实现团队群聊显示

--- imports/client/views/chat/chatSideLeft/GroupList.jsx
+++ imports/client/views/chat/chatSideLeft/GroupList.jsx
@@ -3,12 +3,15 @@ import pureRender from 'pure-render-decorator';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import { Menu } from 'antd';
 
 import Icon from '../../../components/Icon';
 import Avatar from '../../../components/Avatar';
 import UserUtil from '../../../../util/user';
 import Group from '../../../../schema/group';
+import Company from '../../../../schema/company';
 
+const SubMenu = Menu.SubMenu;
 @pureRender
 class GroupList extends Component {
     static propTypes = {
@@ -24,6 +27,10 @@ class GroupList extends Component {
             isShowMyGroup: false,
         };
     }
+    handleClick = (e) => {
+        this.props.handleClick();
+        this.props.changeTo(e.key, e.key, 'groupId', 'message');
+    }
     handleShowCompany = () => {
         this.setState({
             isShowCompany: !this.state.isShowCompany,
@@ -34,49 +41,43 @@ class GroupList extends Component {
             isShowMyGroup: !this.state.isShowMyGroup,
         });
     }
+
+    renderSubMenu = (departments) => {
+        if (departments && departments.length) {
+            return departments.map(dev =>
+                <Menu.Item key={dev._id}>{dev.name}</Menu.Item>,
+            );
+        }
+    }
+    renderTeam = company => <Menu.Item key={company._id}>{company.name}</Menu.Item>
+
+    renderTeamTitle = (name, logo) => <div className="team-title" style={{ display: 'flex' }}><Avatar name="企业" avatarColor="red" avatar={logo} /><p>{name}</p></div>
     render() {
         return (
             <div className="ejianlian-chat-group-list">
                 <div className="chat-friend-pannel">
                     <div className="friend-pannel-type" />
-                    <div className="friend-pannel-list">
-                        <div className="friend-list-item">
-                            <p className="my-chat-group">
-                                <Icon icon="icon-qunzu icon" />
-                            </p>
-                            <p className="friend-name" style={{ borderWidth: this.state.isShowMyGroup ? '0px' : this.props.teamGroups.length > 0 ? '1px' : '0px' }}>
-                                团队群聊
-                                {
-                                    this.state.isShowMyGroup ?
-                                        <Icon icon="icon-xiangshangjiantou-copy-copy-copy icon" onClick={this.handleShowMyGroup} />
-                                        :
-                                        <Icon icon="icon-jiantou-copy icon" onClick={this.handleShowMyGroup} />
-                                }
-                            </p>
-                        </div>
-                        <div style={{ display: this.state.isShowMyGroup ? 'none' : 'block' }}>
+                    <div className="team-organization">
+                        <Menu
+                            onClick={this.handleClick}
+                            mode="inline"
+                            className="team-menu"
+                        >
                             {
-                                this.props.teamGroups.map((item, index) => (
-                                    item ?
-                                        <div
-                                            key={index}
-                                            className="friend-list-item"
-                                            onClick={() => {
-                                                this.props.handleClick();
-                                                this.props.changeTo(item._id, item._id, 'groupId', 'message');
-                                            }}
-                                        >
-                                            <p>
-                                                <Avatar name={item.name} avatar={item.avatar ? item.avatar : 'http://oxldjnom8.bkt.clouddn.com/groupAvatar.png'} />
-                                            </p>
-                                            <p className={this.props.teamGroups.length - 1 !== index ? 'friend-name' : 'friend-name last-type-name'}>{item.name}</p>
-                                        </div>
-                                        :
-                                        null
-                                ),
+                                this.props.teamGroups[0] && this.props.teamGroups.map(company =>
+                                    (<SubMenu
+                                        title={this.renderTeamTitle(company.name, company.avatar)}
+                                        key={company._id}
+                                    >
+                                        {this.renderTeam(company)}
+                                        {
+                                            this.renderSubMenu(company.subGroup)
+                                        }
+                                    </SubMenu>
+                                    ),
                                 )
                             }
-                        </div>
+                        </Menu>
                     </div>
                     <div className="friend-pannel-type" />
                     <div className="friend-pannel-list">
@@ -126,10 +127,17 @@ class GroupList extends Component {
 
 export default withTracker(() => {
     Meteor.subscribe('group');
+    Meteor.subscribe('company');
     const groupIds = UserUtil.getGroups();
     const groups = groupIds.map(_id => Group.findOne({ _id }));
     const selfGroups = groups.filter(x => x && (x.type === 'group' || x.type === undefined)); // 之后数据库重新更新后,要删掉对undefined的判断
     const teamGroups = groups.filter(x => x && (x.type === 'team' && x.companyId));
+    teamGroups.forEach((team) => {
+        const subGroupIds = Company.findOne({ _id: team.companyId }).subGroupIds;
+        if (subGroupIds) {
+            team.subGroup = subGroupIds.map(subGroupId => Group.findOne({ _id: subGroupId }));
+        }
+    });
     return {
         selfGroups,
         teamGroups,
