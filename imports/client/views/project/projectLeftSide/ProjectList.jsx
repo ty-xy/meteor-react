b4a32b6b67d98c6a0d0004cf7b0d50d123ce16da
zg xy <zg-ty@zg18.com>
2017-12-02 22:05:38 +0800

    feat:只有项目创建者和项目成员可以看到已经创建项目

--- imports/client/views/project/projectLeftSide/ProjectList.jsx
+++ imports/client/views/project/projectLeftSide/ProjectList.jsx
@@ -8,12 +8,14 @@ import DefaultProject from './DefaultProject';
 import MyIcon from '../../../components/Icon';
 
 import Project from '../../../../../imports/schema/project';
+import ProjectMember from '../../../../../imports/schema/projectmember';
 
 @pureRender
 class ProjectList extends Component {
     static propTypes = {
         showProject: PropTypes.func,
-        projects: PropTypes.arrayOf(PropTypes.object),
+        project: PropTypes.arrayOf(PropTypes.object),
+        // projects: PropTypes.arrayOf(PropTypes.object),
     }
     constructor(...props) {
         super(...props);
@@ -34,41 +36,42 @@ class ProjectList extends Component {
                     <Link to="/project/task">
                         <DefaultProject click={this.handleClick} />
                     </Link> : null}
-                <ul >
-                    {
-                        this.props.projects.map((value) => {
-                            if (value.headPortrait && value.headPortrait.indexOf('icon') === -1 && value.pigeonhole === 1) {
-                                return (
-                                    <Link to={`/project/task/${value.uprojectId}`} key={value._id}>
-                                        <li className="list-item" key={value._id}>
-                                            <div className="list-img">
-                                                {<img src={`http://oxldjnom8.bkt.clouddn.com//${value.headPortrait}`} alt="" />}
-                                            </div>
-                                            <div className="list-right">
-                                                <p>{value.name}</p>
-                                            </div>
-                                        </li>
-                                    </Link>
-                                );
-                            } else if (value.headPortrait && value.headPortrait.indexOf('icon') === 0 && value.pigeonhole === 1) {
-                                return (
-                                    <Link to={`/project/task/${value.uprojectId}`} key={value._id}>
-                                        <li className="list-item" key={value._id}>
-                                            <div className="list-img-icon">
-                                                {<MyIcon icon={`${value.headPortrait} icon`} size={30} iconColor="#ddd" />}
-                                            </div>
-                                            <div className="list-right">
-                                                <p>{value.name}</p>
-                                            </div>
-                                        </li>
-                                    </Link>
-                                );
-                            }
-                            return null;
-                        })
+                {this.props.project && this.props.project.length > 0 ?
+                    <ul >
+                        {
+                            this.props.project.map((value) => {
+                                if (value.headPortrait && value.headPortrait.indexOf('icon') === -1 && value.pigeonhole === 1) {
+                                    return (
+                                        <Link to={`/project/task/${value.uprojectId}`} key={value._id}>
+                                            <li className="list-item" key={value._id}>
+                                                <div className="list-img">
+                                                    {<img src={`http://oxldjnom8.bkt.clouddn.com//${value.headPortrait}`} alt="" />}
+                                                </div>
+                                                <div className="list-right">
+                                                    <p>{value.name}</p>
+                                                </div>
+                                            </li>
+                                        </Link>
+                                    );
+                                } else if (value.headPortrait && value.headPortrait.indexOf('icon') === 0 && value.pigeonhole === 1) {
+                                    return (
+                                        <Link to={`/project/task/${value.uprojectId}`} key={value._id}>
+                                            <li className="list-item" key={value._id}>
+                                                <div className="list-img-icon">
+                                                    {<MyIcon icon={`${value.headPortrait} icon`} size={30} iconColor="#ddd" />}
+                                                </div>
+                                                <div className="list-right">
+                                                    <p>{value.name}</p>
+                                                </div>
+                                            </li>
+                                        </Link>
+                                    );
+                                }
+                                return null;
+                            })
 
-                    }
-                </ul>
+                        }
+                    </ul> : null}
             </div >
         );
     }
@@ -76,9 +79,18 @@ class ProjectList extends Component {
 
 export default withTracker(() => {
     Meteor.subscribe('project');
-    const projects = Project.find({}, { sort: { createTime: -1 } }).fetch();
-    console.log(projects[0]);
+    Meteor.subscribe('projectmember');
+    const projectId = ProjectMember.find({ member: Meteor.userId() }).fetch();
+    console.log(projectId);
+    const project = [];
+    projectId.map((item) => {
+        const project1 = Project.find({ uprojectId: item.projectId }, { sort: { createTime: -1 } }).fetch();
+        return project.unshift(project1[0]);
+    });
+    const projects = Project.find({ }, { sort: { createTime: -1 } }).fetch();
+    console.log(project);
     return {
         projects,
+        project,
     };
 })(ProjectList);
