ec8a449a1a654a59cf67685c1eb7e7015a3f1d9a
zg xy <zg-ty@zg18.com>
2017-12-25 18:10:04 +0800

    fix:更改index.js

--- imports/client/views/Index.jsx
+++ imports/client/views/Index.jsx
@@ -8,12 +8,13 @@ class Index extends Component {
     static propTypes = {
         history: PropTypes.object,
         location: PropTypes.object,
+        hash: PropTypes.string,
     }
     componentWillMount() {
         Meteor.autorun(() => {
             const user = Meteor.user();
             const { history, location } = this.props;
-            if (!user && this.isLogin === true) {
+            if (!user && this.isLogin === true && this.props.hash === '') {
                 this.isLogin = false;
                 if (location.search && location.search.indexOf('companyId')) {
                     history.push({ pathname: '/login', search: location.search, state: 'invite' });
@@ -26,6 +27,13 @@ class Index extends Component {
                     history.push({ pathname: '/chat', search: location.search, state: location.state });
                 } else {
                     history.push('/chat');
+                }
+            } else if (!user && this.isLogin === true && this.props.hash === '#id') {
+                // this.isLogin = false;
+                if (location.search && location.search.indexOf('companyId')) {
+                    history.push({ pathname: '/register', search: location.search, state: 'invite' });
+                } else {
+                    history.push('/register');
                 }
             }
         });
