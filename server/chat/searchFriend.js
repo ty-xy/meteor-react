7bef7880a57a0374d860d5ed60e6a57b05202c64
zg xy <zg-ty@zg18.com>
2018-01-29 13:10:14 +0800

    add:增加查询好友的方法

--- server/chat/searchFriend.js
+++ server/chat/searchFriend.js
@@ -10,4 +10,12 @@ Meteor.methods({
         assert(profile.friends.indexOf(findResult._id) === -1, 400, '该用户已经是您的好友了');
         return findResult;
     },
+    searchFriends(username) {
+        const findResult = Meteor.users.findOne({ username });
+        // assert(findResult !== undefined, 400, '搜索不到该用户');
+        // assert(findResult._id !== Meteor.userId(), 400, '不能添加自己为好友');
+        // const { profile = {} } = Meteor.user() || {};
+        // assert(profile.friends.indexOf(findResult._id) === -1, 400, '该用户已经是您的好友了');
+        return findResult;
+    },
 });
